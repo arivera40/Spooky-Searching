dbd0e4a5980b1accb130265be1e9f0bcf03824e1
Andy J Rivera <rivera.andy40@gmail.com>
2019-11-19 18:54:11 -0500

    Update multitest_thread.c

--- multitest_thread.c
+++ multitest_thread.c
@@ -5,13 +5,37 @@
 #include<pthread.h>
 #include<unistd.h>	
 
+typedef struct Arguments{
+	int start;
+	int end;
+	int target;
+}Arguments;
+
+int *list;	//made this global so my splitSearch function has access to it
+
+void * splitSearch(void *args){
+	Arguments *arguments = (Arguments*)args;
+	int *theStatus;
+	int result = -1;
+	int i;
+	for(i=arguments->start; i < arguments->end; i++){
+		if(list[i] == arguments->target){
+			result = i;
+			break;
+		}
+	}
+	theStatus = &result;
+	free(arguments);
+	pthread_exit((void*)theStatus);
+	return (void*)theStatus;
+}
 //This function will act as main function
-int findTarget_thread(int target, int size, int *list){
-	int threadNum = size / 100;
-	int split = size / threadNum;
-	void *status;
+int findTarget_thread(int target, int size, int threadNum, int split,int *array){
 	pthread_t threads[threadNum];
+	list = array;
+	void *status;
 	int *exitStatus = (int*)malloc(sizeof(int) * threadNum);
+	//Arguments *args = (Arguments *)malloc(sizeof(Arguments) * threadNum);
 
 	int i;
 	int start;
@@ -25,30 +49,19 @@ int findTarget_thread(int target, int size, int *list){
 			start = end;
 			end = start + size;
 		}
-		pthread_create(&threads[i], NULL, splitSearch, start, end, target, list);
+		Arguments *args = (Arguments*)malloc(sizeof(Arguments));
+		args->start = start;
+		args->end = end;
+		args->target = target;
+		pthread_create(&threads[i], NULL, splitSearch, (void*)args);
 	}
 	for(i=0; i < threadNum; i++){
-		pthread_join(thread[i], &status);
+		pthread_join(threads[i], &status);
 		int *result = (int*)status;
 		exitStatus[i] = *result;
-		if(*exitStatus[i] != -1){
-			return *exitStatus[i];
+		if(exitStatus[i] != -1){
+			return exitStatus[i];
 		}
 	}
 	return -1;
-}
-
-void * splitSearch(int start, int end, int target, int *list){
-	int *theStatus;
-	int result = -1;
-	int i;
-	for(i=start, i < end; i++){
-		if(list[i] == target){
-			result = i;
-			break;
-		}
-	}
-	theStatus = &result;
-	pthread_exit((void*)theStatus);
-	return (void*)theStatus;
 }
