e363fe8d9c6e9ecc994c3c463856f3669118514e
Andy J Rivera <rivera.andy40@gmail.com>
2019-11-19 20:48:40 -0500

    Update searchtest.c

--- searchtest.c
+++ searchtest.c
@@ -1,10 +1,9 @@
+#include<stdlib.h>
+#include<stddef.h>
 #include<stdio.h>
 #include<time.h>
 #include <pthread.h>
 #include"multitest.h"
-
-//pthread_t threadID[2];
-//pthread_mutex_t lock;
 
 int randomize(int size){
 	return 1 + (rand() % size);
@@ -13,16 +12,25 @@ int randomize(int size){
 int main(int argc, char* argv){
 	int *list = (int*)malloc(sizeof(int) * 20000);	//declaration for list of values (size sucesptible to change)
 	int i;
+	int j;
+	int sizes[80];
+	int workloadTimes[80];
+	int totalTime;
+	struct timeval start;
+	struct timeval end;
 	for(i=0; i < 20000; i++){
 		list[i] = i+1;
 	}
 
 	int random;
 	int size = 0;
+	int childrenNum = 0;
+	int split = 0;
 	int target = randomize(250);	//Chooses random number to search for as target
 	int targetIndex = -1;		//Keeps track of target's index in order to swap for next test
 	while(size < 20000){	//20,000 / 250 = 80 iterations
 		size += 250;
+		sizes[j] = size;
 		if(targetIndex != -1){
 			random = randomize(size)-1;
 			int swap = list[random];
@@ -35,22 +43,19 @@ int main(int argc, char* argv){
 			list[random] = list[i];
 			list[i] = temp;
 		}
-		targetIndex = findTarget(target, 250, list);
-	}
+		childrenNum = size / 25;
+		split = size/childrenNum;
 
-/*	int error;
-	if(pthread_mutex_init(&lock, NULL) != 0){
-		printf("\n mutex init has failed\n");
-		return 1;
+		gettimeofday(&start, 0);
+		targetIndex = findTarget(target, size, childrenNum, split, list);
+		gettimeofday(&end, 0);
+		workloadTimes[j] = ((end.tv_sec - start.tv_sec)*1000000 + (end.tv_usec-start.tv_usec));
+		totalTime += workloadTimes[j];
+		j++;
 	}
-	int i;
-	for(i=0; i < 2; i++){
-		error = pthread_create(&(threadID[i]), NULL, &test_thread, &lock);
-		if(error != 0){
-			printf("\nThread can't be created: [%s]", strerror(error));
-		}
+	for(i=0; i < 80; i++){
+		printf("Array of size %d takes %d milliseconds to locate target\n", sizes[i], workloadTimes[i]);
 	}
-	pthread_join(threadID[0], NULL);
-	pthread_join(threadID[1], NULL);
-	pthread_mutex_destroy(&lock);	*/
+	printf("Average time to locate target: %d milliseconds\n", totalTime/80);
+	return 0;
 }
